89791eee940f814b217ca20a4ba5cca1d3828f95
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-03-23 16:07:19 +0530

    Time: 20 ms (17.56%), Space: 24.2 MB (5.18%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -9,40 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 #define ll unsigned long long int
-
 class Solution {
-    
-    vector<vector<ll>> tree;
-    
-    void solve(TreeNode* root, ll lvl, ll idx) {
-        
-        if(root == NULL) return;
-        
-        if(lvl == tree.size()) tree.push_back({});
-        
-        tree[lvl].push_back(idx);
-        
-        solve(root->left,  lvl + 1, 2 * idx);
-        solve(root->right, lvl + 1, 2 * idx + 1);
-        
+    void solve(map<ll,set<ll>>&ans,TreeNode*root,ll ans1,ll l){
+        if(root==NULL)
+            return;
+        if(ans.find(l)==ans.end()){
+            ans[l]={ans1};
+        }else{
+            ans[l].insert(ans1);
+        }
+        solve(ans,root->left,2*ans1,l+1);
+        solve(ans,root->right,2*ans1+1,l+1);
     }
-    
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        
-        solve(root, 0, 0);
-        
-        ll ans = 0;
-        
-        for(auto &v : tree) {
-            
-            ll cur = v.back() - v[0] + 1;
-            
-            ans = max(ans, cur);
+        ll ans1=0,l=0;
+        map<ll,set<ll>>ans;
+        ll maxi=0;
+        if(root==NULL)
+            return maxi;
+        solve(ans,root,ans1,l);
+        for(auto it:ans){
+            auto e=it.second;
+            maxi=max(maxi,*e.rbegin()-*e.begin()+1);
         }
-        
-        return ans;
+        return maxi;
     }
 };
