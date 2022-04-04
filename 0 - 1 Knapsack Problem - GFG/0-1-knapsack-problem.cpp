b3d1f2cdc1646395d85b53165e938139e220a4e6
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-04-04 20:15:15 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int dp[1001][1001];
+    Solution(){
+        memset(dp,-1,sizeof(dp));
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        if(n==0 || W==0)
+            return 0;
+        if(dp[n][W]!=-1)
+            return dp[n][W];
+        
+        if(wt[n-1]<=W)
+           return dp[n][W]=max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1),knapSack(W,wt,val,n-1));
+        else
+           return dp[n][W]=knapSack(W,wt,val,n-1);
+       
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
