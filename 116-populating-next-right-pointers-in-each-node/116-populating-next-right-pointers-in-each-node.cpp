213196f1438001e2fbee96a589bd220689a04145
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-04-05 18:47:03 +0530

    Time: 29 ms (42.20%), Space: 16.8 MB (67.73%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -17,34 +17,29 @@ public:
 */
 
 class Solution {
+    void sol(Node*root,Node*parent,bool exist){
+        if(root==NULL)
+            return;
+        if(exist){
+            root->next=parent->right;
+        }else{
+            if(parent==root){
+                root->next=NULL;
+            }
+            else if(parent->next){
+                root->next=parent->next->left;
+            }else{
+                root->next=NULL;
+            }
+        }
+        sol(root->left,root,true);
+        sol(root->right,root,false);
+    }
 public:
     Node* connect(Node* root) {
         if(root==NULL)
             return NULL;
-        vector<pair<Node*,int>>v;
-        queue<pair<Node*,int>>q;
-        q.push({root,0});
-        while(!q.empty()){
-            pair<Node*,int>p=q.front();
-            q.pop();
-            Node*f=p.first;
-            int lvl=p.second;
-            v.push_back({f,lvl});
-            if(f->left)
-                q.push({f->left,lvl+1});
-            if(f->right)
-                q.push({f->right,lvl+1});
-        }
-        
-        for(int i=0;i<v.size()-1;i++){
-            cout<<v[i].first->val<<" ";
-            if(v[i].second==v[i+1].second){
-                v[i].first->next=v[i+1].first;
-            }else{
-                v[i].first->next=NULL;
-            }
-        }
-
+        sol(root,root,false);
         return root;
     }
 };
