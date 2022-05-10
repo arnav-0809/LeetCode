ea395c40997aa86880bdd8deeb084c70ca01d359
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-05-10 17:04:21 +0530

    Time: 38 ms (31.79%), Space: 13.5 MB (79.92%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    void bfs(int i,vector<vector<int>>& graph,vector<int>&vis,bool&ans){
-        queue<int>q;
-        q.push(i);
-        while(!q.empty()){
-            int f=q.front();
-            q.pop();
-            for(auto it:graph[f]){
-                if(vis[it]==-3 && vis[f]==-1){
-                    q.push(it);
-                    vis[it]=-2;
-                }else if(vis[it]==-3 && vis[f]==-2){
-                    q.push(it);
-                    vis[it]=-1;
-                }else if((vis[it]==-2 && vis[f]==-2) ||(vis[it]==-1 && vis[f]==-1)){
-                    ans=false;
-                    return;
-                }
+    void dfs(int i,vector<vector<int>>& graph,vector<int>&vis,bool&ans){
+        for(auto it:graph[i]){
+            if(vis[it]==-3 && vis[i]==-1){
+                vis[it]=-2;
+                dfs(it,graph,vis,ans);
+            }else if(vis[it]==-3 && vis[i]==-2){
+                vis[it]=-1;
+                dfs(it,graph,vis,ans);
+            }else if((vis[it]==-2 && vis[i]==-2) ||(vis[it]==-1 && vis[i]==-1)){
+                ans=false;
+                return;
             }
         }
-        ans=true;
     }
     
     bool isBipartite(vector<vector<int>>& graph) {
-        bool ans=false;
+        bool ans=true;
         vector<int>vis(graph.size(),-3);
         for(int i=0;i<graph.size();i++){
             if(vis[i]==-3){
                 vis[i]=-2;
-                bfs(i,graph,vis,ans);
+                dfs(i,graph,vis,ans);
                 if(ans==false){
                     return false;
                 }
