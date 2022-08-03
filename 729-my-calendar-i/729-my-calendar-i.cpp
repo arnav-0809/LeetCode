60f3a94e70846ebe856e8a781b751938f4662bc3
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-08-04 00:30:17 +0530

    Time: 119 ms (80.53%), Space: 38 MB (76.14%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,16 +1,51 @@
 class MyCalendar {
-    map<int,int>m;
+    class booking{
+        public:
+            int start,end;
+            booking *left = NULL;
+            booking *right = NULL;
+            booking(int start, int end){
+                this->start = start;
+                this->end = end;
+            }
+    };
+    
+    booking*root;
 public:
     MyCalendar() {
-        
+        root=new booking(0,0);
     }
     
     bool book(int start, int end) {
-        auto it=m.upper_bound(start);
-        if(it==m.end() || end<=it->second){
-            m[end]=start;
+        if(root->start==0 && root->end==0){
+            root->start=start;
+            root->end=end;
             return true;
         }
+        return insert(root,start,end);
+    }
+    
+    bool insert(booking*root,int start,int end){
+        if(!root)
+            return false;
+        if(root->start>=end){
+            if(root->left)
+                return insert(root->left,start,end);
+            else{
+                root->left=new booking(start,end);
+                return true;
+            }
+        }
+        
+        if(root->end<=start){
+            if(root->right)
+                return insert(root->right,start,end);
+            else{
+                root->right=new booking(start,end);
+                return true;
+            }
+        }
+        
         return false;
     }
 };
