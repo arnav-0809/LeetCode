c20b41b0afd16e8028874727c77c26cbb5a3c82e
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-08-18 01:02:16 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int x=m+n-2;
-        double ans=1;
         int y=n-1;
+        double ans=1;
         for(int i=1;i<=y;i++){
             ans=ans*(x-y+i)/i;
         }
-        return (int)ans;
+        return (double)ans;
     }
 };
