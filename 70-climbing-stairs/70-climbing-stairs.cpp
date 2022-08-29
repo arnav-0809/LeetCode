f499df980f052fc24cec74f87cc408f08b49cc3c
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-08-29 21:43:02 +0530

    Time: 3 ms (36.11%), Space: 6 MB (57.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int sol(int n,vector<int>&dp){
-        if(n==0 || n==1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=sol(n-1,dp)+sol(n-2,dp);
-    }
-    
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return sol(n,dp);
+        int prev=1,prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr= prev+prev1;
+            prev=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
