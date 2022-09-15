238a0a6015b734b979c168ac52bef065c5e2a765
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-09-16 00:52:47 +0530

    Time: 5 ms (49.74%), Space: 6.1 MB (9.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int sol(int n,vector<int>&dp){
-        if(n<=1){
-            return n;
-        }
-        
-        if(dp[n]!=-1)return dp[n];
-        
-        return dp[n] = sol(n-1,dp) + sol(n-2,dp);
-    }
-    
     int fib(int n) {
+        if(n==0)
+            return 0;
         vector<int>dp(n+1,-1);
-        return sol(n,dp);
+        dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
