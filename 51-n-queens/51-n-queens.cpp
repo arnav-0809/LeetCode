dddb1b72e45935a722ac2d2b29239b21add82fd9
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-09-19 22:59:15 +0530

    Time: 7 ms (78.68%), Space: 7.3 MB (66.29%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,65 +1,47 @@
 class Solution {
 public:
-    bool check(vector<string>&s,int row,int col,int n){
-        int row1 = row, col1 = col;
-        while(row >= 0 && col >= 0)
-        {    
-            if(s[row][col] == 'Q')
-                return false;
-            
-            row--, col--;
-        }
-        
-        row = row1, col = col1;
-        while(col >= 0)
-        {    
-            if(s[row][col] == 'Q')
-                return false;
-            
-            col--;
-        }
-        
-        row = row1, col = col1;
-        while(row < n && col >= 0)
+    void sol(vector<vector<string>>&ans, vector<string>&s, vector<int>&lowerD, vector<int>&upperD,                    vector<int>&leftRow, int n, int col)
+    {
+        if(col == n)
         {
-            if(s[row][col] == 'Q')
-                return false;
-            
-            row++, col--;
-        }
-        
-        return true;
-    }
-    
-    void sol(vector<vector<string>>&ans, vector<string>&s, int n, int col){
-        if(n == col){
             ans.push_back(s);
             return;
         }
         
         for(int i = 0; i < n; i++)
         {
-            if(check(s, i, col, n))
+            if(lowerD[i + col] == 0 && upperD[n - 1 + col - i] == 0 && leftRow[i] == 0)
             {
                 s[i][col] = 'Q';
-                sol(ans, s, n, col+1);
+                lowerD[i + col] = 1;
+                upperD[n - 1 + col - i] = 1;
+                leftRow[i] = 1;
+                
+                sol(ans, s, lowerD, upperD, leftRow, n, col + 1);
+                
                 s[i][col] = '.';
+                lowerD[i + col] = 0;
+                upperD[n - 1 + col - i] = 0;
+                leftRow[i] = 0;
             }
         }
-        
     }
     
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n) 
+    {
         vector<vector<string>>ans;
         string a = "";
         
-        for(int i=0; i<n; i++)
+        for(int i = 0; i < n; i++)
         {
             a += '.';
         }
         
         vector<string>s(n, a);
-        sol(ans, s, n, 0);
+        vector<int>lowerD(2 * n - 1, 0), upperD(2 * n - 1, 0), leftRow(n, 0);
+        
+        sol(ans, s, lowerD, upperD, leftRow, n , 0);
+        
         return ans;
     }
 };
