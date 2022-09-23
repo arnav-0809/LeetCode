e0d980cd13b6c4ea5e7e284165c25dec9f078651
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-09-23 22:41:11 +0530

    Time: 7 ms (12.90%), Space: 7.8 MB (26.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -5,10 +5,11 @@ public:
         vector<int>dp(n,-1);
         dp[0]=nums[0];
         for(int i=1;i<n;i++){
+            int pick = nums[i];
             if(i>1)
-                dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-            else
-                dp[i]=max(nums[i],dp[i-1]);
+                pick += dp[i-2];
+            int notPick = dp[i-1];
+            dp[i]=max(pick,notPick);
         }
        return dp[n-1];
     }
