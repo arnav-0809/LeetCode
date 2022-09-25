56599015adb184153c12209d38e6dac900cfb1ce
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-09-25 20:00:17 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (13.34%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int sol(int m, int n, vector<vector<int>>&dp,vector<vector<int>>&og){
-        if(m<0 || n<0) 
-            return 0;
-        
-        if(og[m][n]==1)
-            return 0;
-        
-        if(m==0 && n==0)
-            return 1;
-        
-        if(dp[m][n]!=-1)return dp[m][n];
-        
-        int up=sol(m-1,n,dp,og);
-        int left=sol(m,n-1,dp,og);
-        return dp[m][n]=up+left;
-    }
-    
     int uniquePathsWithObstacles(vector<vector<int>>& og) {
         int m = og.size();
         int n = og[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return sol(m-1,n-1,dp,og);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (og[i][j]==1)dp[i][j]= 0;
+                else if(i==0 && j==0)dp[0][0]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0)
+                        up = dp[i-1][j];
+                    if(j>0)
+                        left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
