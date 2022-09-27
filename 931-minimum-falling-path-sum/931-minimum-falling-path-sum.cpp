55eccd9c4e1f811562411c314a449bbf98d4a7da
Arnav Sehgal <55274224+arnav-0809@users.noreply.github.com>
2022-09-27 23:39:14 +0530

    Time: 37 ms (17.52%), Space: 10.3 MB (54.14%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int sol(int m,int n,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        if(m<0 || n<0 || n>=matrix.size()){
-            return 1e9;
-        }
-        
-        if(m==0){
-            return matrix[0][n];
-        }
-        
-        if(dp[m][n]!=-101)return dp[m][n];
-        
-        int up=matrix[m][n]+sol(m-1,n,matrix,dp);
-        int upLeft =matrix[m][n]+sol(m-1,n-1,matrix,dp);
-        int upRight=matrix[m][n]+sol(m-1,n+1,matrix,dp);
-        
-        return dp[m][n]=min(up,min(upLeft,upRight));  
-    }
-    
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int mini=1e9;
-         vector<vector<int>>dp(m,vector<int>(m,-101)); 
-        for(int i=0;i<m;i++){
-            mini=min(mini,sol(m-1,i,matrix,dp));
+        int m = matrix.size();
+        int mini = 1e9;
+        vector<vector<int>>dp(m, vector<int>(m, -101));
+        
+        for(int i = 0; i < m; i++)
+        {
+            dp[0][i] = matrix[0][i];
         }
+        
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int up = matrix[i][j] + dp[i-1][j];
+                
+                int upLeft = matrix[i][j];
+                if(j == 0)
+                {
+                    upLeft += 1e9;
+                }
+                else
+                {
+                    upLeft += dp[i-1][j-1];
+                }
+                
+                int upRight = matrix[i][j];
+                if(j == m-1)
+                {
+                    upRight += 1e9;
+                }
+                else
+                {
+                    upRight += dp[i-1][j+1];
+                }
+                
+                dp[i][j] = min(up, min(upLeft, upRight)); 
+            }
+        }
+        
+        for(int i = 0; i < m; i++)
+        {
+            mini = min(mini, dp[m-1][i]);
+        }
+        
         return mini;
     }
 };
